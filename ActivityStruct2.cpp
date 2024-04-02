f3cddafff0c9a57184b3bed9a66c78081060e436
pisboiii <hfzfahrzi@gmail.com>
2024-04-01 21:15:59 -0700

    Add project files.

---
+++ ActivityStruct2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+	string desa;
+	string kota;
+};
+
+
+
+struct mahasiswa {
+	string nim;
+	string nama;
+	DetailAlamat alamat;
+	int umur;
+};
+
+int main() {
+	mahasiswa  mhs;
+	cout << "nomor Mahasiswa : ";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa :";
+	cin >> mhs.nama;
+
+	cout << "alamat mahasiswa : ";
+	cout << "\t nama Desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\t Nama kota : ";
+	cin >> mhs.alamat.kota;
+
+	cout << "Umur mahasiswa : ";
+	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n Nim :" << mhs.nim;
+	cout << "\n Nama : " << mhs.nama;
+	cout << "\n alamat : ";
+	cout << "\n Desa : " << mhs.alamat.desa;
+	cout << "\n Kota : " << mhs.alamat.kota;
+	cout << "\n Umur : " << mhs.umur;
+}
---
+++ ActivityStruct_0181.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa {
+    string nim;
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main()
+{
+    mahasiswa mhs;
+    cout << "Nomor Mahasiswa: ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa: ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa: ";
+    cin >> mhs.alamat;
+    cout <<"Umur Mahasiswa: ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n Nim: " << mhs.nim;
+    cout << "\n Nama: " << mhs.nim;
+    cout << "\n Alamat: " << mhs.nim;
+    cout << "\n Umur: " << mhs.nim;
+}
+
+
